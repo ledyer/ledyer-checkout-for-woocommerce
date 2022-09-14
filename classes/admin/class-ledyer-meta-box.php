d70e6f6baccc672506f59d77be73616c09b71cd4
perjo927 <djpjgj@gmail.com>
2022-09-14 21:19:32 +0200

    Revert "fix: sec level 300 attest redirect + on hold status"
    This reverts commit 8c04e22601de2535ea952fc82cd9622aa30872fe.

--- classes/admin/class-ledyer-meta-box.php
+++ classes/admin/class-ledyer-meta-box.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Meta box
  *
@@ -8,24 +7,21 @@
  * @package Ledyer
  * @since   1.0.0
  */
-
 namespace Ledyer\Admin;
 
-\defined('ABSPATH') || die();
+\defined( 'ABSPATH' ) || die();
 
 /**
  * Meta_Box class.
  *
  * Handles the meta box for LCO
  */
-class Meta_Box
-{
+class Meta_Box {
 	/**
 	 * Class constructor.
 	 */
-	public function __construct()
-	{
-		add_action('add_meta_boxes', array($this, 'add_meta_boxes'));
+	public function __construct() {
+		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
 	}
 
 	/**
@@ -34,13 +30,12 @@ class Meta_Box
 	 * @param string $post_type The WordPress post type.
 	 * @return void
 	 */
-	public function add_meta_boxes($post_type)
-	{
-		if ('shop_order' === $post_type) {
+	public function add_meta_boxes( $post_type ) {
+		if ( 'shop_order' === $post_type ) {
 			$order_id = get_the_ID();
-			$order    = wc_get_order($order_id);
-			if (in_array($order->get_payment_method(), array('lco', 'ledyer_payments'), true)) {
-				add_meta_box('lco_meta_box', __('Ledyer Order Info', 'ledyer-checkout-for-woocommerce'), array($this, 'meta_box_content'), 'shop_order', 'side', 'core');
+			$order    = wc_get_order( $order_id );
+			if ( in_array( $order->get_payment_method(), array( 'lco', 'ledyer_payments' ), true ) ) {
+				add_meta_box( 'lco_meta_box', __( 'Ledyer Order Info', 'ledyer-checkout-for-woocommerce' ), array( $this, 'meta_box_content' ), 'shop_order', 'side', 'core' );
 			}
 		}
 	}
@@ -50,24 +45,21 @@ class Meta_Box
 	 *
 	 * @return void
 	 */
-	public function meta_box_content()
-	{
+	public function meta_box_content() {
 		$order_id = get_the_ID();
-		$order    = wc_get_order($order_id);
+		$order    = wc_get_order( $order_id );
 
 		// False if automatic settings are enabled, true if not. If true then show the option.
-		if (!empty(get_post_meta($order_id, '_transaction_id', true)) && !empty(get_post_meta($order_id, '_wc_ledyer_order_id', true))) {
+		if ( ! empty( get_post_meta( $order_id, '_transaction_id', true ) ) && ! empty( get_post_meta( $order_id, '_wc_ledyer_order_id', true ) ) ) {
 
-			// TODO: if ledyer_pending or status on-hold: get info from session
-			// Then: print_standard_content but with session info
-			$ledyer_order = ledyer()->api->get_order(get_post_meta($order_id, '_wc_ledyer_order_id', true));
+			$ledyer_order = ledyer()->api->get_order( get_post_meta( $order_id, '_wc_ledyer_order_id', true ) );
 
-			if (is_wp_error($ledyer_order)) {
-				$this->print_error_content(__('Failed to retrieve the order from Ledyer.', 'ledyer-checkout-for-woocommerce'));
+			if ( is_wp_error( $ledyer_order ) ) {
+				$this->print_error_content( __( 'Failed to retrieve the order from Ledyer.', 'ledyer-checkout-for-woocommerce' ) );
 				return;
 			}
 		}
-		$this->print_standard_content($ledyer_order);
+		$this->print_standard_content( $ledyer_order );
 	}
 
 	/**
@@ -76,33 +68,32 @@ class Meta_Box
 	 * @param object $ledyer_order The Ledyer order object.
 	 * @return void
 	 */
-	public function print_standard_content($ledyer_order)
-	{
+	public function print_standard_content( $ledyer_order ) {
 		// Show ledyer order information.
-?>
-		<div class="lco-meta-box-content">
-			<?php if ($ledyer_order) { ?>
+		?>
+        <div class="lco-meta-box-content">
+			<?php if ( $ledyer_order ) { ?>
 				<?php
-				if ('' !== ledyer()->get_setting('testmode')) {
+				if ( '' !== ledyer()->get_setting('testmode') ) {
 					$environment = 'yes' === ledyer()->get_setting('testmode') ? 'Sandbox' : 'Production';
-				?>
-					<strong><?php esc_html_e('Ledyer Environment: ', 'ledyer-checkout-for-woocommerce'); ?> </strong><?php echo esc_html($environment); ?><br />
+					?>
+                    <strong><?php esc_html_e( 'Ledyer Environment: ', 'ledyer-checkout-for-woocommerce' ); ?> </strong><?php echo esc_html( $environment ); ?><br/>
 				<?php } ?>
-				<strong><?php esc_html_e('Customer Organization number: ', 'ledyer-checkout-for-woocommerce'); ?> </strong> <?php echo esc_html($ledyer_order['customer']['companyId']); ?><br />
-				<strong><?php esc_html_e('Order id: ', 'ledyer-checkout-for-woocommerce'); ?> </strong> <?php echo esc_html($ledyer_order['id']); ?><br />
-				<strong><?php esc_html_e('Ledyer id: ', 'ledyer-checkout-for-woocommerce'); ?> </strong> <?php echo esc_html($ledyer_order['orderReference']); ?><br />
-				<?php if (!empty($ledyer_order['customer']['firstName']) || !empty($ledyer_order['customer']['lastName'])) : ?>
-					<strong><?php esc_html_e('Order setter: ', 'ledyer-checkout-for-woocommerce'); ?> </strong> <?php echo esc_html($ledyer_order['customer']['firstName'] . ' ' . $ledyer_order['customer']['lastName']); ?><br />
+                <strong><?php esc_html_e( 'Customer Organization number: ', 'ledyer-checkout-for-woocommerce' ); ?> </strong> <?php echo esc_html( $ledyer_order['customer']['companyId'] ); ?><br/>
+                <strong><?php esc_html_e( 'Order id: ', 'ledyer-checkout-for-woocommerce' ); ?> </strong> <?php echo esc_html( $ledyer_order['id'] ); ?><br/>
+                <strong><?php esc_html_e( 'Ledyer id: ', 'ledyer-checkout-for-woocommerce' ); ?> </strong> <?php echo esc_html( $ledyer_order['orderReference'] ); ?><br/>
+				<?php if( ! empty( $ledyer_order['customer']['firstName'] ) || ! empty( $ledyer_order['customer']['lastName'] ) ) : ?>
+                    <strong><?php esc_html_e( 'Order setter: ', 'ledyer-checkout-for-woocommerce' ); ?> </strong> <?php echo esc_html( $ledyer_order['customer']['firstName'] . ' ' . $ledyer_order['customer']['lastName'] ); ?><br/>
 				<?php endif; ?>
-				<?php if ($ledyer_order['customer']['reference1']) : ?>
-					<strong><?php esc_html_e('Customer referens 1: ', 'ledyer-checkout-for-woocommerce'); ?> </strong> <?php echo esc_html($ledyer_order['customer']['reference1']); ?><br />
+				<?php if( $ledyer_order['customer']['reference1'] ) : ?>
+                    <strong><?php esc_html_e( 'Customer referens 1: ', 'ledyer-checkout-for-woocommerce' ); ?> </strong> <?php echo esc_html( $ledyer_order['customer']['reference1'] ); ?><br/>
 				<?php endif; ?>
-				<?php if ($ledyer_order['customer']['reference2']) : ?>
-					<strong><?php esc_html_e('Customer referens 2: ', 'ledyer-checkout-for-woocommerce'); ?> </strong> <?php echo esc_html($ledyer_order['customer']['reference2']); ?><br />
+				<?php if( $ledyer_order['customer']['reference2'] ) : ?>
+                    <strong><?php esc_html_e( 'Customer referens 2: ', 'ledyer-checkout-for-woocommerce' ); ?> </strong> <?php echo esc_html( $ledyer_order['customer']['reference2'] ); ?><br/>
 				<?php endif; ?>
 			<?php } ?>
-		</div>
-	<?php
+        </div>
+		<?php
 	}
 
 	/**
@@ -111,12 +102,11 @@ class Meta_Box
 	 * @param string $message The error message.
 	 * @return void
 	 */
-	public function print_error_content($message)
-	{
-	?>
-		<div class="lco-meta-box-content">
-			<p><?php echo esc_html($message); ?></p>
-		</div>
-<?php
+	public function print_error_content( $message ) {
+		?>
+        <div class="lco-meta-box-content">
+            <p><?php echo esc_html( $message ); ?></p>
+        </div>
+		<?php
 	}
 }
--- includes/lco-functions.php
+++ includes/lco-functions.php
@@ -1,104 +1,100 @@
 <?php
-
 /**
  * Functions file for the plugin.
  *
  * @package Ledyer
  */
 
-\defined('ABSPATH') || die();
+\defined( 'ABSPATH' ) || die();
 
 /**
  * Gets a Ledyer order. Either creates or updates existing order.
  *
  * @return array
  */
-function lco_create_or_update_order()
-{
+function lco_create_or_update_order() {
 	// Need to calculate these here, because WooCommerce hasn't done it yet.
 	WC()->cart->calculate_fees();
 	WC()->cart->calculate_shipping();
 	WC()->cart->calculate_totals();
 
-	$old_ledyer_settings = WC()->session->get('lco_wc_settings');
+	$old_ledyer_settings = WC()->session->get( 'lco_wc_settings' );
 
-	if (
-		WC()->session->get('lco_wc_order_id')
-		&& $old_ledyer_settings['allow_custom_shipping'] === ledyer()->get_setting('allow_custom_shipping')
-		&& $old_ledyer_settings['customer_show_name_fields'] === ledyer()->get_setting('customer_show_name_fields')
-		&& $old_ledyer_settings['terms_url'] === ledyer()->get_setting('terms_url')
-		&& $old_ledyer_settings['privacy_url'] === ledyer()->get_setting('privacy_url')
-	) {
+	if ( WC()->session->get( 'lco_wc_order_id' )
+	     && $old_ledyer_settings['allow_custom_shipping'] === ledyer()->get_setting( 'allow_custom_shipping' )
+	     && $old_ledyer_settings['customer_show_name_fields'] === ledyer()->get_setting( 'customer_show_name_fields' )
+	     && $old_ledyer_settings['terms_url'] === ledyer()->get_setting( 'terms_url' )
+	     && $old_ledyer_settings['privacy_url'] === ledyer()->get_setting( 'privacy_url' ) ) {
 
 
-		$ledyer_order_id   = WC()->session->get('lco_wc_order_id');
-		$ledyer_session_id = WC()->session->get('lco_wc_session_id');
+		$ledyer_order_id   = WC()->session->get( 'lco_wc_order_id' );
+		$ledyer_session_id = WC()->session->get( 'lco_wc_session_id' );
 		$data              = \Ledyer\Requests\Helpers\Woocommerce_Bridge::get_updated_cart_data();
-		$ledyer_order      = ledyer()->api->update_order_session($ledyer_order_id, $data);
+		$ledyer_order      = ledyer()->api->update_order_session( $ledyer_order_id, $data );
 
-		if (!$ledyer_order || (is_object($ledyer_order) && 'WP_Error' === get_class($ledyer_order)) || $ledyer_order['orderId'] !== $ledyer_order_id || $ledyer_order['sessionId'] !== $ledyer_session_id) {
+		if ( ! $ledyer_order || ( is_object( $ledyer_order ) && 'WP_Error' === get_class( $ledyer_order ) ) || $ledyer_order['orderId'] !== $ledyer_order_id || $ledyer_order['sessionId'] !== $ledyer_session_id ) {
 			// If update order failed try to create new order.
 			$data         = \Ledyer\Requests\Helpers\Woocommerce_Bridge::get_cart_data();
-			$ledyer_order = ledyer()->api->create_order_session($data);
-			if (!$ledyer_order || (is_object($ledyer_order) && 'WP_Error' === get_class($ledyer_order))) {
+			$ledyer_order = ledyer()->api->create_order_session( $data );
+			if ( ! $ledyer_order || ( is_object( $ledyer_order ) && 'WP_Error' === get_class( $ledyer_order ) ) ) {
 				// If failed then bail.
-				if (is_object($ledyer_order) && 'WP_Error' === get_class($ledyer_order)) {
+				if( is_object( $ledyer_order ) && 'WP_Error' === get_class( $ledyer_order ) ) {
 					$errors = $ledyer_order->errors;
 				} else {
 					$errors = $ledyer_order;
 				}
 
-				\Ledyer\Logger::log($errors);
+				\Ledyer\Logger::log( $errors );
 				return false;
 			}
-			WC()->session->set('lco_wc_session_id', $ledyer_order['sessionId']);
-			WC()->session->set('lco_wc_order_id', $ledyer_order['orderId']);
-			WC()->session->set('lco_wc_settings', array(
-				'allow_custom_shipping'     => ledyer()->get_setting('allow_custom_shipping'),
-				'customer_show_name_fields' => ledyer()->get_setting('customer_show_name_fields'),
-				'terms_url'                 => ledyer()->get_setting('terms_url'),
-				'privacy_url'               => ledyer()->get_setting('privacy_url'),
-			));
+			WC()->session->set( 'lco_wc_session_id', $ledyer_order['sessionId'] );
+			WC()->session->set( 'lco_wc_order_id', $ledyer_order['orderId'] );
+			WC()->session->set( 'lco_wc_settings', array(
+				'allow_custom_shipping'     => ledyer()->get_setting( 'allow_custom_shipping' ),
+				'customer_show_name_fields' => ledyer()->get_setting( 'customer_show_name_fields' ),
+				'terms_url'                 => ledyer()->get_setting( 'terms_url' ),
+				'privacy_url'               => ledyer()->get_setting( 'privacy_url' ),
+			) );
 
 			return $ledyer_order;
 		} else {
 			//If sessions somehow change??
-			if (($ledyer_order_id !== $ledyer_order['orderId']) || ($ledyer_session_id !== $ledyer_order['sessionId'])) {
-				WC()->session->set('lco_wc_session_id', $ledyer_order['sessionId']);
-				WC()->session->set('lco_wc_order_id', $ledyer_order['orderId']);
-				WC()->session->set('lco_wc_settings', array(
-					'allow_custom_shipping'     => ledyer()->get_setting('allow_custom_shipping'),
-					'customer_show_name_fields' => ledyer()->get_setting('customer_show_name_fields'),
-					'terms_url'                 => ledyer()->get_setting('terms_url'),
-					'privacy_url'               => ledyer()->get_setting('privacy_url'),
-				));
+			if ( ( $ledyer_order_id !== $ledyer_order['orderId'] ) || ( $ledyer_session_id !== $ledyer_order['sessionId'] ) ) {
+				WC()->session->set( 'lco_wc_session_id', $ledyer_order['sessionId'] );
+				WC()->session->set( 'lco_wc_order_id', $ledyer_order['orderId'] );
+				WC()->session->set( 'lco_wc_settings', array(
+					'allow_custom_shipping'     => ledyer()->get_setting( 'allow_custom_shipping' ),
+					'customer_show_name_fields' => ledyer()->get_setting( 'customer_show_name_fields' ),
+					'terms_url'                 => ledyer()->get_setting( 'terms_url' ),
+					'privacy_url'               => ledyer()->get_setting( 'privacy_url' ),
+				) );
 			}
 		}
 		return $ledyer_order;
 	} else {
 		// Create new order, since we dont have one.
 		$data         = \Ledyer\Requests\Helpers\Woocommerce_Bridge::get_cart_data();
-		$ledyer_order = ledyer()->api->create_order_session($data);
+		$ledyer_order = ledyer()->api->create_order_session( $data );
 
-		if (!$ledyer_order || (is_object($ledyer_order) && 'WP_Error' === get_class($ledyer_order))) {
-			if (is_object($ledyer_order) && 'WP_Error' === get_class($ledyer_order)) {
+		if ( ! $ledyer_order || ( is_object( $ledyer_order ) && 'WP_Error' === get_class( $ledyer_order ) ) ) {
+			if( is_object( $ledyer_order ) && 'WP_Error' === get_class( $ledyer_order ) ) {
 				$errors = $ledyer_order->errors;
 			} else {
 				$errors = $ledyer_order;
 			}
 
-			\Ledyer\Logger::log($errors);
+			\Ledyer\Logger::log( $errors );
 			return false;
 		}
 
-		WC()->session->set('lco_wc_session_id', $ledyer_order['sessionId']);
-		WC()->session->set('lco_wc_order_id', $ledyer_order['orderId']);
-		WC()->session->set('lco_wc_settings', array(
-			'allow_custom_shipping'     => ledyer()->get_setting('allow_custom_shipping'),
-			'customer_show_name_fields' => ledyer()->get_setting('customer_show_name_fields'),
-			'terms_url'                 => ledyer()->get_setting('terms_url'),
-			'privacy_url'               => ledyer()->get_setting('privacy_url'),
-		));
+		WC()->session->set( 'lco_wc_session_id', $ledyer_order['sessionId'] );
+		WC()->session->set( 'lco_wc_order_id', $ledyer_order['orderId'] );
+		WC()->session->set( 'lco_wc_settings', array(
+			'allow_custom_shipping'     => ledyer()->get_setting( 'allow_custom_shipping' ),
+			'customer_show_name_fields' => ledyer()->get_setting( 'customer_show_name_fields' ),
+			'terms_url'                 => ledyer()->get_setting( 'terms_url' ),
+			'privacy_url'               => ledyer()->get_setting( 'privacy_url' ),
+		) );
 
 		return $ledyer_order;
 	}
@@ -108,9 +104,8 @@ function lco_create_or_update_order()
  *
  * @return boolean
  */
-function is_lco_confirmation()
-{
-	if (isset($_GET['confirm']) && 'yes' === $_GET['confirm'] && isset($_GET['lco_wc_session_id'])) { // phpcs:ignore WordPress.Security.NonceVerification -- No nonce possible on this page.
+function is_lco_confirmation() {
+	if ( isset( $_GET['confirm'] ) && 'yes' === $_GET['confirm'] && isset( $_GET['lco_wc_session_id'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification -- No nonce possible on this page.
 		return true;
 	}
 
@@ -120,15 +115,14 @@ function is_lco_confirmation()
 /**
  * Get the selected, or the first, payment method.
  */
-function lco_wc_get_selected_payment_method()
-{
+function lco_wc_get_selected_payment_method() {
 	$selected_payment_method = '';
-	if (null !== WC()->session && method_exists(WC()->session, 'get') && WC()->session->get('chosen_payment_method')) {
-		$selected_payment_method = WC()->session->get('chosen_payment_method');
+	if ( null !== WC()->session && method_exists( WC()->session, 'get' ) && WC()->session->get( 'chosen_payment_method' ) ) {
+		$selected_payment_method = WC()->session->get( 'chosen_payment_method' );
 	} else {
 		$available_payment_gateways = WC()->payment_gateways->get_available_payment_gateways();
-		reset($available_payment_gateways);
-		$selected_payment_method = key($available_payment_gateways);
+		reset( $available_payment_gateways );
+		$selected_payment_method = key( $available_payment_gateways );
 	}
 
 	return $selected_payment_method;
@@ -139,32 +133,30 @@ function lco_wc_get_selected_payment_method()
  *
  * @return void
  */
-function lco_unset_sessions()
-{
-	WC()->session->__unset('lco_wc_order_id');
-	WC()->session->__unset('lco_wc_session_id');
-	WC()->session->__unset('lco_wc_settings');
+function lco_unset_sessions() {
+	WC()->session->__unset( 'lco_wc_order_id' );
+	WC()->session->__unset( 'lco_wc_session_id' );
+	WC()->session->__unset( 'lco_wc_settings' );
 }
 
 
 /**
  * Shows select another payment method button in Ledyer Checkout page.
  */
-function lco_wc_show_another_gateway_button()
-{
+function lco_wc_show_another_gateway_button() {
 	$available_gateways = WC()->payment_gateways()->get_available_payment_gateways();
 
-	if (count($available_gateways) > 1) {
-		$settings                   = get_option('woocommerce_lco_settings');
-		$select_another_method_text = isset($settings['select_another_method_text']) && '' !== $settings['select_another_method_text'] ? $settings['select_another_method_text'] : __('Select another payment method', 'ledyer-checkout-for-woocommerce');
+	if ( count( $available_gateways ) > 1 ) {
+		$settings                   = get_option( 'woocommerce_lco_settings' );
+		$select_another_method_text = isset( $settings['select_another_method_text'] ) && '' !== $settings['select_another_method_text'] ? $settings['select_another_method_text'] : __( 'Select another payment method', 'ledyer-checkout-for-woocommerce' );
 
-?>
-		<p class="ledyer-checkout-select-other-wrapper">
-			<a class="checkout-button button" href="#" id="ledyer-checkout-select-other">
-				<?php echo esc_html($select_another_method_text); ?>
-			</a>
-		</p>
-<?php
+		?>
+        <p class="ledyer-checkout-select-other-wrapper">
+            <a class="checkout-button button" href="#" id="ledyer-checkout-select-other">
+				<?php echo esc_html( $select_another_method_text ); ?>
+            </a>
+        </p>
+		<?php
 	}
 }
 
@@ -175,73 +167,69 @@ function lco_wc_show_another_gateway_button()
  *
  * @return void
  */
-// /class-ledyer-confirmation.php(55): wc_ledyer_confirm_ledyer_order('71', true)
-function wc_ledyer_confirm_ledyer_order($order_id, $ledyer_pending)
-{
-	$order         = wc_get_order($order_id);
-	$payment_id    = get_post_meta($order_id, '_wc_ledyer_order_id', true);
-	$session_id    = get_post_meta($order_id, '_wc_ledyer_session_id', true);
+function wc_ledyer_confirm_ledyer_order( $order_id, $ledyer_pending ) {
+	$order         = wc_get_order( $order_id );
+	$payment_id    = get_post_meta( $order_id, '_wc_ledyer_order_id', true );
+	$session_id    = get_post_meta( $order_id, '_wc_ledyer_session_id', true );
 
-	if (null === $payment_id) {
-		$payment_id = WC()->session->get('lco_wc_order_id');
+	if ( null === $payment_id ) {
+		$payment_id = WC()->session->get( 'lco_wc_order_id' );
 	}
 
-	if (null === $session_id) {
-		$session_id = WC()->session->get('lco_wc_session_id');
-	}
+	if( null === $session_id ) {
+		$session_id = WC()->session->get( 'lco_wc_session_id' );
+    }
 
-	if (!$ledyer_pending) {
-		$request = ledyer()->api->get_order($payment_id);
+	if( ! $ledyer_pending ) {
+		$request = ledyer()->api->get_order( $payment_id );
 	} else {
-		$request = ledyer()->api->get_order_session($payment_id);
+		$request = ledyer()->api->get_order_session( $session_id );
 	}
 
-	if (isset($request['orderAmount']) || isset($request['id'])) {
+	if ( isset( $request['orderAmount'] ) || isset( $request['id'] ) ) {
 
-		do_action('ledyer_process_payment', $order_id, $request);
+		do_action( 'ledyer_process_payment', $order_id, $request );
 
-		update_post_meta($order_id, 'ledyerpayment_type', $request['paymentMethod']['type']);
-		update_post_meta($order_id, 'ledyer_payment_method', $request['paymentMethod']['provider']);
-		update_post_meta($order_id, '_ledyer_date_paid', gmdate('Y-m-d H:i:s'));
+		update_post_meta( $order_id, 'ledyerpayment_type', $request['paymentMethod']['type'] );
+		update_post_meta( $order_id, 'ledyer_payment_method', $request['paymentMethod']['provider'] );
+		update_post_meta( $order_id, '_ledyer_date_paid', gmdate( 'Y-m-d H:i:s' ) );
 
 		$isAdvanceInvoice = $request['paymentMethod']['type'] == 'advanceInvoice';
 
-		if (!$ledyer_pending) {
-			$order->add_order_note(sprintf(__('New payment created in Ledyer with Payment ID %1$s. Payment type - %2$s. Awaiting charge.', 'ledyer-checkout-for-woocommerce'), $payment_id, $request['paymentMethod']['type']));
+		if( ! $ledyer_pending ) {
+			$order->add_order_note( sprintf( __( 'New payment created in Ledyer with Payment ID %1$s. Payment type - %2$s. Awaiting charge.', 'ledyer-checkout-for-woocommerce' ), $payment_id, $request['paymentMethod']['type'] ) );
 
-			if (!$isAdvanceInvoice) {
-				$order->payment_complete($payment_id);
+			if ( ! $isAdvanceInvoice) {
+				$order->payment_complete( $payment_id );
 			}
-			$response = ledyer()->api->acknowledge_order($payment_id);
-			if (is_wp_error($response)) {
-				\Ledyer\Logger::log('Couldn\'t acknowledge order ' . $payment_id);
+			$response = ledyer()->api->acknowledge_order( $payment_id );
+			if( is_wp_error( $response ) ) {
+				\Ledyer\Logger::log( 'Couldn\'t acknowledge order ' . $payment_id  );
 			}
 		} else {
-			// TODO: set status "on hold" + populate order info (now we get "Failed to retrieve the order from Ledyer")
-			$order->update_status('on-hold');
-			$order->add_order_note(sprintf(__('New payment created in Ledyer with Payment ID %1$s. Payment type - %2$s. Awaiting signature.', 'ledyer-checkout-for-woocommerce'), $session_id, $request['paymentMethod']['type']));
-		}
+			$order->add_order_note( sprintf( __( 'New payment created in Ledyer with Payment ID %1$s. Payment type - %2$s. Awaiting signature.', 'ledyer-checkout-for-woocommerce' ), $session_id, $request['paymentMethod']['type'] ) );
+        }
+
 	} else {
 		// Purchase not finalized in Ledyer.
 		// If this is a redirect checkout flow let's redirect the customer to cart page.
-		wp_safe_redirect(html_entity_decode($order->get_cancel_order_url()));
+		wp_safe_redirect( html_entity_decode( $order->get_cancel_order_url() ) );
 		exit;
 	}
 }
 
 
-function wc_ledyer_cart_redirect()
-{
+function wc_ledyer_cart_redirect() {
 	$url = add_query_arg(
 		array(
 			'lco-order' => 'error',
-			'reason'    => base64_encode(__('Failed to load Ledyer Checkout template file.', 'ledyer-checkout-for-woocommerce')),
+			'reason'    => base64_encode( __( 'Failed to load Ledyer Checkout template file.', 'ledyer-checkout-for-woocommerce' ) ),
 			// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions
 		),
 		wc_get_cart_url()
 	);
 
 
-	wp_safe_redirect($url);
+	wp_safe_redirect( $url );
 	exit;
 }
