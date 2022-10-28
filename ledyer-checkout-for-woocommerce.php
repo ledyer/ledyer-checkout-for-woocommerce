be65522aea5fe4a43a7ad5beda1641290030f473
Programmer Per <djpjgj@gmail.com>
2022-10-28 19:47:26 +0200

    fix: client validation (#14)

--- ledyer-checkout-for-woocommerce.php
+++ ledyer-checkout-for-woocommerce.php
@@ -5,7 +5,7 @@
  * Description: Ledyer Checkout payment gateway for WooCommerce.
  * Author: Maksimer/Ledyer
  * Author URI: https://www.maksimer.com/
- * Version: 1.1.0
+ * Version: 1.1.1
  * Text Domain: ledyer-checkout-for-woocommerce
  * Domain Path: /languages
  *
