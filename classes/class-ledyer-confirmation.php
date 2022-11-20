a24b612a13a7fbfd410c356790ec4976d8db638f
perjo927 <djpjgj@gmail.com>
2022-11-20 16:50:13 +0100

    wip

--- classes/class-ledyer-confirmation.php
+++ classes/class-ledyer-confirmation.php
@@ -33,18 +33,19 @@ class Confirmation {
 	 */
 	public function confirm_order() {
 
+		$ledyer_purchase_complete = filter_input( INPUT_GET, 'lco_purchase_complete', FILTER_SANITIZE_STRING );
 		$ledyer_confirm = filter_input( INPUT_GET, 'lco_confirm', FILTER_SANITIZE_STRING );
 		$order_key    = filter_input( INPUT_GET, 'key', FILTER_SANITIZE_STRING );
 		$ledyer_pending    = filter_input( INPUT_GET, 'lco_pending', FILTER_SANITIZE_STRING );
 
-		if ( empty( $ledyer_confirm ) || empty( $order_key ) ) {
+		if ( empty( $ledyer_confirm ) || empty( $order_key ) || empty( $ledyer_purchase_complete) ) {
 			return;
 		}
 		$order_id = wc_get_order_id_by_order_key( $order_key );
 		$order    = wc_get_order( $order_id );
 
 		$order_pending_order = ( 'yes' === $ledyer_pending ) ? true : false;
-		$should_confirm_order = ( 'yes' === $ledyer_confirm) ? true : false;
+		$should_confirm_order = ( 'yes' === $ledyer_purchase_complete ) ? true : false;
 
 		Logger::log( $order_id . ': Confirm the Ledyer order from the confirmation page.' );
 
