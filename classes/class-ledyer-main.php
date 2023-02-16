44e3ae58a2d84cf9e9b653ee22caca3de9538273
Peter Larsson <peter@pln.se>
2023-02-16 07:49:16 +0100

    version bump

--- classes/class-ledyer-main.php
+++ classes/class-ledyer-main.php
@@ -51,7 +51,7 @@ class Ledyer_Checkout_For_WooCommerce {
 	 */
 	public $checkout;
 
-	const VERSION = '1.5.0';
+	const VERSION = '1.5.1';
 	const SLUG = 'ledyer-checkout-for-woocommerce';
 	const SETTINGS = 'ledyer_checkout_for_woocommerce_settings';
 
