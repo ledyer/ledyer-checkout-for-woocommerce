e432ad2e8660b577ce13682f2035b2a5ca8a0125
Peter Larsson <peter@pln.se>
2023-05-22 13:25:27 +0200

    chore: bump version

--- ledyer-checkout-for-woocommerce.php
+++ ledyer-checkout-for-woocommerce.php
@@ -5,7 +5,7 @@
  * Description: Ledyer Checkout payment gateway for WooCommerce.
  * Author: Maksimer/Ledyer
  * Author URI: https://www.maksimer.com/
- * Version: 1.6.2
+ * Version: 1.6.3
  * Text Domain: ledyer-checkout-for-woocommerce
  * Domain Path: /languages
  *
