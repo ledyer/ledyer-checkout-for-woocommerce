c56446dd6eb24451f9b98668278483d0062fd808
Jens Filipsson <jensfilipsson@gmail.com>
2023-07-07 15:51:31 +0200

    chore: gitattributes (#50)

--- ledyer-checkout-for-woocommerce.php
+++ ledyer-checkout-for-woocommerce.php
@@ -5,7 +5,7 @@
  * Description: Ledyer Checkout payment gateway for WooCommerce.
  * Author: Maksimer/Ledyer
  * Author URI: https://www.maksimer.com/
- * Version: $Format:%(describe:tags=HEAD)$
+ * Version: 1.6.8
  * Text Domain: ledyer-checkout-for-woocommerce
  * Domain Path: /languages
  *
