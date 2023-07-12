b4e19b793b652a26561ef4ea15105ba06e68ee20
Jens Filipsson <jensfilipsson@gmail.com>
2023-07-12 15:04:14 +0200

    Fix/prerelease (#63)

---
+++ release.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  branches: [
+    "main",
+    { name: "alpha", prerelease: "alpha" },
+    { name: "beta", prerelease: "beta" },
+  ],
+  plugins: [
+    ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
+    [
+      "@semantic-release/release-notes-generator",
+      { preset: "conventionalcommits" },
+    ],
+    "@semantic-release/changelog",
+    [
+      "semantic-release-plugin-update-version-in-files",
+      {
+        files: [
+          "ledyer-checkout-for-woocommerce.php",
+          "readme.txt",
+          "classes/class-ledyer-main.php",
+          "languages/ledyer-checkout-for-woocommerce.pot",
+        ],
+        placeholder: "0.0.0-development",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
