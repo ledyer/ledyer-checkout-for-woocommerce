89f71787462f563cb91b59fd2291cc995b540d05
Jens Filipsson <jensfilipsson@gmail.com>
2023-07-12 12:26:16 +0200

    fix: add workflow that doesn't require commits to repo (#59)

--- release.config.js
+++ release.config.js
@@ -19,20 +19,6 @@ module.exports = {
         placeholder: "0.0.0-development",
       },
     ],
-    [
-      "@semantic-release/git",
-      {
-        assets: [
-          "ledyer-checkout-for-woocommerce.php",
-          "readme.txt",
-          "classes/class-ledyer-main.php",
-          "languages/ledyer-checkout-for-woocommerce.pot",
-          "CHANGELOG.md",
-        ],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
     "@semantic-release/github",
   ],
 };
