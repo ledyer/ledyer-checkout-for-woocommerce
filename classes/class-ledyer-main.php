1027e29fc6e17eed0559bae17aa04756d48bb120
Tim Jackleus <timjackleus@gmail.com>
2023-10-20 16:41:38 +0200

    fix: bump version (#88)

--- classes/class-ledyer-main.php
+++ classes/class-ledyer-main.php
@@ -51,7 +51,7 @@ class Ledyer_Checkout_For_WooCommerce {
 	 */
 	public $checkout;
 
-	const VERSION = '1.7.4';
+	const VERSION = '1.7.5';
 	const SLUG = 'ledyer-checkout-for-woocommerce';
 	const SETTINGS = 'ledyer_checkout_for_woocommerce_settings';
 
