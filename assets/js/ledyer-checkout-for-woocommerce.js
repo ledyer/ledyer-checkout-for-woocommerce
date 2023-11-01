e12d73cbe3dc09badb0990f91baf06a70adfb5ac
Simon Solders <simon.solders@gmail.com>
2023-11-01 21:55:01 +0100

    fix: strip away duplicate order comments field

--- assets/js/ledyer-checkout-for-woocommerce.js
+++ assets/js/ledyer-checkout-for-woocommerce.js
@@ -139,6 +139,10 @@ jQuery(function ($) {
          * Moves all non standard fields to the extra checkout fields.
          */
         moveExtraCheckoutFields: function () {
+            if (document.querySelectorAll("#order_comments_field").length > 1) {
+                document.querySelector("#order_comments_field").remove();
+            }
+
 
             // Move order comments.
             $('.woocommerce-additional-fields').appendTo('#lco-extra-checkout-fields');
