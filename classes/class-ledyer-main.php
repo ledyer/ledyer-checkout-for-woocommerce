6526dfab8a6d935af0c75fd84c22d3567961de5e
Simon Solders <simon.solders@gmail.com>
2023-11-02 09:00:27 +0100

    fix: versions 1.7.8 (#92)

--- classes/class-ledyer-main.php
+++ classes/class-ledyer-main.php
@@ -51,7 +51,7 @@ class Ledyer_Checkout_For_WooCommerce {
 	 */
 	public $checkout;
 
-	const VERSION = '1.7.7';
+	const VERSION = '1.7.8';
 	const SLUG = 'ledyer-checkout-for-woocommerce';
 	const SETTINGS = 'ledyer_checkout_for_woocommerce_settings';
 
