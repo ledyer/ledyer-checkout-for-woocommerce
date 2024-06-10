3c19f0d4c56d3583402ea982fcca68b33f530a1b
timjackleus <timjackleus@gmail.com>
2024-06-10 21:19:21 +0200

    format everything with wp-scripts

--- release.config.js
+++ release.config.js
@@ -1,29 +1,32 @@
 module.exports = {
-  branches: [
-    "main",
-    { name: "alpha", prerelease: "alpha" },
-    { name: "beta", prerelease: "beta" },
-  ],
-  plugins: [
-    ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
-    [
-      "@semantic-release/release-notes-generator",
-      { preset: "conventionalcommits" },
-    ],
-    "@semantic-release/changelog",
-    [
-      "semantic-release-plugin-update-version-in-files",
-      {
-        files: [
-          "ledyer-checkout-for-woocommerce.php",
-          "readme.txt",
-          "classes/class-ledyer-main.php",
-          "languages/ledyer-checkout-for-woocommerce.pot",
-        ],
-        placeholder: "0.0.0-development",
-      },
-    ],
-    "semantic-release-export-data",
-    "@semantic-release/github",
-  ],
+	branches: [
+		'main',
+		{ name: 'alpha', prerelease: 'alpha' },
+		{ name: 'beta', prerelease: 'beta' },
+	],
+	plugins: [
+		[
+			'@semantic-release/commit-analyzer',
+			{ preset: 'conventionalcommits' },
+		],
+		[
+			'@semantic-release/release-notes-generator',
+			{ preset: 'conventionalcommits' },
+		],
+		'@semantic-release/changelog',
+		[
+			'semantic-release-plugin-update-version-in-files',
+			{
+				files: [
+					'ledyer-checkout-for-woocommerce.php',
+					'readme.txt',
+					'classes/class-ledyer-main.php',
+					'languages/ledyer-checkout-for-woocommerce.pot',
+				],
+				placeholder: '0.0.0-development',
+			},
+		],
+		'semantic-release-export-data',
+		'@semantic-release/github',
+	],
 };
