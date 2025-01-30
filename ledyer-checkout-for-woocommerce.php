5b61fec89bf42d0d09d0c70fad366db15d7e0abb
timjackleus <timjackleus@gmail.com>
2025-01-30 09:44:07 +0100

    Version 1.11.0

--- ledyer-checkout-for-woocommerce.php
+++ ledyer-checkout-for-woocommerce.php
@@ -5,7 +5,7 @@
  * Description: Ledyer Checkout payment gateway for WooCommerce.
  * Author: Ledyer
  * Author URI: https://www.ledyer.com/
- * Version: 1.10.2
+ * Version: 1.11.0
  * Text Domain: ledyer-checkout-for-woocommerce
  * Domain Path: /languages
  *
